e0d654864adfe4a34f2495100ed193227e4464c9
Rasmus Plauborg <rpl@mjolner.dk>
2021-12-11 21:26:25 +0100

    Extremely basic error status page.

--- web-app/src/main/kotlin/dk/mailr/webApp/application.kt
+++ web-app/src/main/kotlin/dk/mailr/webApp/application.kt
@@ -7,12 +7,13 @@ import dk.mailr.pokerApplication.pokerRouting
 import dk.mailr.pokerInfrastructure.di.pokerModule
 import dk.mailr.webApp.di.mediatorModule
 import io.ktor.application.Application
+import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.application.log
 import io.ktor.features.CallLogging
-import io.ktor.features.ContentNegotiation
-import io.ktor.jackson.jackson
-import io.ktor.locations.Locations
+import io.ktor.features.StatusPages
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
 import io.ktor.routing.routing
 import org.koin.core.logger.Level
 import org.koin.ktor.ext.inject
@@ -23,6 +24,15 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module(dbConnectionString: String = environment.config.property("mongodb.uri").getString()) {
     install(CallLogging)
+    install(StatusPages) {
+        exception<Throwable> {
+            call.respond(HttpStatusCode.InternalServerError, "Something went wrong, excuse us!")
+            throw it // otherwise, exception will be swallowed
+            // A better solution besides of course handling more specific exceptions, is to use HTML pages,
+            // see https://ktor.io/docs/status-pages.html#statusfile
+        }
+    }
+
     koin {
         slf4jLogger(Level.ERROR) // Must be ERROR until https://github.com/InsertKoinIO/koin/issues/1188 is fixed
         modules(
