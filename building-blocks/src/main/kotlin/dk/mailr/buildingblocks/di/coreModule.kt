395c5b7aae06e86b8573f2835d34c4c24f546e85
Rasmus Plauborg <rpl@mjolner.dk>
2022-02-15 08:03:55 +0100

    TODO for scoping DI stuff.

--- building-blocks/src/main/kotlin/dk/mailr/buildingblocks/di/coreModule.kt
+++ building-blocks/src/main/kotlin/dk/mailr/buildingblocks/di/coreModule.kt
@@ -14,6 +14,7 @@ import org.koin.dsl.module
 import org.litote.kmongo.KMongo
 
 fun coreModule(dbConnectionString: String) = module {
+    // TODO scoping, currently everything is singleton
     single<MongoClient> {
         KMongo.createClient(
             MongoClientSettings.builder()
