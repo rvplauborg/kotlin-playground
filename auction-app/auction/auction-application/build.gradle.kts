bc6ac6434882cd6f9ec05501c1909e41f4a1fd40
Rasmus Plauborg <rpl@mjolner.dk>
2022-02-16 10:54:14 +0100

    Remove not needed dependencies.

--- auction-app/auction/auction-application/build.gradle.kts
+++ auction-app/auction/auction-application/build.gradle.kts
@@ -9,6 +9,4 @@ dependencies {
     implementation("io.ktor:ktor-server-core:$ktorVersion")
 
     testImplementation(testFixtures(projects.buildingBlocks))
-    integrationTestImplementation("io.ktor:ktor-server-tests:$ktorVersion")
-    integrationTestImplementation("org.testcontainers:mongodb:1.16.2")
 }
