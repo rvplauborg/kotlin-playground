e720f21d844a7950e83c5ab433a5d6c5bcfd24e3
Rasmus Plauborg <rpl@mjolner.dk>
2022-02-17 18:50:28 +0100

    Fix detekt issues.

--- auction-app/auction/auction-application/src/main/kotlin/dk/mailr/auctionApplication/AuctionRepository.kt
+++ auction-app/auction/auction-application/src/main/kotlin/dk/mailr/auctionApplication/AuctionRepository.kt
@@ -3,4 +3,4 @@ package dk.mailr.auctionApplication
 import dk.mailr.auctionDomain.Auction
 import dk.mailr.buildingblocks.dataAccess.EntityRepository
 
-interface AuctionRepository: EntityRepository<Auction>
+interface AuctionRepository : EntityRepository<Auction>
