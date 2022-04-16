ddc2e06bacc0230a1cc5a6689f2a5360fc7fcaa2
Rasmus Plauborg <rpl@mjolner.dk>
2022-04-16 18:13:43 +0200

    Clarifying KDoc.

--- building-blocks/src/main/kotlin/dk/mailr/buildingblocks/di/KoinExtensions.kt
+++ building-blocks/src/main/kotlin/dk/mailr/buildingblocks/di/KoinExtensions.kt
@@ -2,4 +2,7 @@ package dk.mailr.buildingblocks.di
 
 import org.koin.core.scope.Scope
 
+/**
+ * [T] must be injectable for the extension to work as intended.
+ */
 inline fun <reified T : Any> Scope.classToLazyProvider() = T::class.java to inject<T>()
